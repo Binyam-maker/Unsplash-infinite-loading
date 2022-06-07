f58f9421c1303891d680427042d7cf13173b520c
Binyam Sintayehu <73512283+Binyam-maker@users.noreply.github.com>
2022-06-07 16:36:06 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,131 @@
+import React, { useState, useEffect, useRef } from "react";
+
+// import { Faquery } from "react-icons/fa";
+import Photo from "./Photo";
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
+
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(0);
+  const [query, setQuery] = useState("");
+  const mounted = useRef(false);
+  const [newImages, setNewImages] = useState(false);
+
+  const fetchPhotos = async () => {
+    setLoading(true);
+    let url;
+    const urlPage = `&page=${page}`;
+    const urlQuery = `&query=${query}`;
+
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`;
+    }
+
+    try {
+      console.log(url);
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      setPhotos((oldPhotos) => {
+        if (query && page === 1) {
+          return data.results;
+        }
+        if (query) {
+          return [...oldPhotos, ...data.results];
+        } else {
+          return [...oldPhotos, ...data];
+        }
+      });
+      setNewImages(false);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setNewImages(false);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPhotos();
+  }, [page]);
+
+  useEffect(() => {
+    if (!mounted) {
+      mounted.cuttent = true;
+      return;
+    }
+    if (!newImages) return;
+    if (loading) return;
+    setPage((oldPage) => oldPage + 2);
+  }, [newImages]);
+
+  const event = () => {
+    if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 2) {
+      setNewImages(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", event);
+    return () => window.removeEventListener("scroll", event);
+  }, []);
+
+  // useEffect(() => {
+  //   const event = window.addEventListener("scroll", () => {
+  //     if (
+  //       !loading &&
+  //       window.innerHeight + window.scrollY > document.body.scrollHeight - 2
+  //     ) {
+  //       console.log("hello");
+  //       setPage((oldPage) => {
+  //         return oldPage + 1;
+  //       });
+  //     }
+  //   });
+  //   return () => window.removeEventListener("scroll", event);
+  // }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!query) return;
+
+    if (page === 1) {
+      fetchPhotos();
+      return;
+    }
+    setPage(1);
+  };
+  return (
+    <main>
+      <section className="query">
+        <form className="query-form">
+          <input
+            type="text"
+            className="form-input"
+            placeholder="query"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button type="submit" className="submit-btn" onClick={handleSubmit}>
+            {/* <Faquery /> */}
+          </button>
+        </form>
+      </section>
+      <section className="photos">
+        <div className="photos-center">
+          {photos.map((image, index) => {
+            return <Photo key={image.id} {...image} />;
+          })}
+        </div>
+        {loading && <h2 className="loading">loading...</h2>}
+      </section>
+    </main>
+  );
+}
+
+export default App;
